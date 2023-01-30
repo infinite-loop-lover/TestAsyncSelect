989095689ed6b78cc7e0baf67920ce1260f6a731
Andrus Chen <119281949+AndrusChen@users.noreply.github.com>
2023-01-30 09:19:07 -0800

    Initial commit

---
+++ example.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import AsyncSelect from 'react-select/async';
+import { ColourOption, colourOptions } from './docs/data';
+
+const filterColors = (inputValue: string) => {
+  return colourOptions.filter((i) =>
+    i.label.toLowerCase().includes(inputValue.toLowerCase())
+  );
+};
+
+const loadOptions = (
+  inputValue: string,
+  callback: (options: ColourOption[]) => void
+) => {
+  setTimeout(() => {
+    callback(filterColors(inputValue));
+  }, 1000);
+};
+
+export default () => (
+  <AsyncSelect cacheOptions loadOptions={loadOptions} defaultOptions />
+);
